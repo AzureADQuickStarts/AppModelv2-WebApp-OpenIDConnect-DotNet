6a32ccf48b0ccd533fc9fe390b28ef43044f9769
andretms <andret@microsoft.com>
2018-04-05 14:16:19 -0700

    Fixing claim for username

--- AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/ClaimsController.cs
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/ClaimsController.cs
@@ -20,8 +20,8 @@ namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
             //You get the user’s first and last name below:
             ViewBag.Name = userClaims?.FindFirst("name")?.Value;
 
-            // The 'Name' claim can be used for showing the username
-            ViewBag.Username = userClaims?.FindFirst(System.IdentityModel.Claims.ClaimTypes.Name)?.Value;
+            // The 'preferred_username' claim can be used for showing the username
+            ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
 
             // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
             ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
